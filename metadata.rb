8903aa92a803fc7aad9c1093838ba0da6308d754
Shaun Dern <shaun@substantial.com>
2012-04-05 17:33:18 -0700

    adding depends in meta

--- metadata.rb
+++ metadata.rb
@@ -4,3 +4,7 @@ license          "All rights reserved"
 description      "Installs/Configures phantomjs"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          "0.0.3"
+
+%w{ debian ubuntu }.each do |os|
+  supports os
+end
