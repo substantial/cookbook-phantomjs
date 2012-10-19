305314b1bf3166218d91a7b7b1864a832f877c38
Shaun Dern <shaun@substantial.com>
2012-10-19 10:04:56 -0700

    update phantomjs version

--- attributes/phantomjs.rb
+++ attributes/phantomjs.rb
@@ -1,7 +1,8 @@
-default[:phantomjs][:x86_64][:tar_url]  = "http://phantomjs.googlecode.com/files/phantomjs-1.5.0-linux-x86_64-dynamic.tar.gz"
-default[:phantomjs][:x86_64][:checksum] = "137113dbb25dcd7cf4e19f70c05c11ed8f258b24"
-default[:phantomjs][:x86_64][:filename] = "phantomjs-1.5.0-linux-x86_64-dynamic.tar.gz"
+default[:phantomjs][:x86_64][:tar_url]  = "https://phantomjs.googlecode.com/files/phantomjs-1.7.0-linux-x86_64.tar.bz2"
+default[:phantomjs][:x86_64][:checksum] = "28346300cedc0fbeea4264f124417f2d5ec50210"
+default[:phantomjs][:x86_64][:filename] = "phantomjs-1.7.0-linux-x86_64.tar.bz2"
 
-default[:phantomjs][:x86][:tar_url]     = "http://phantomjs.googlecode.com/files/phantomjs-1.5.0-linux-x86-dynamic.tar.gz"
-default[:phantomjs][:x86][:checksum]    = "fbae9534bb8c9d4ec3ac6fdb194f12de75144219"
-default[:phantomjs][:x86][:filename]    = "phantomjs-1.5.0-linux-x86-dynamic.tar.gz"
+default[:phantomjs][:x86][:tar_url]     = "https://phantomjs.googlecode.com/files/phantomjs-1.7.0-linux-i686.tar.bz2"
+default[:phantomjs][:x86][:checksum]    = "4508abf55fef386a9079ed029b6d36d8719d2686"
+default[:phantomjs][:x86][:filename]    = "phantomjs-1.7.0-linux-i686.tar.bz2"
+
