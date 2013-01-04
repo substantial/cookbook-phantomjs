00493334e3c4fbf54262dd7a753e4f4cc8903078
Joshua Hou <joshua@substantial.com>
2013-01-04 15:54:14 -0800

    bump phantomjs version to 1.8

--- attributes/phantomjs.rb
+++ attributes/phantomjs.rb
@@ -1,6 +1,6 @@
-default[:phantomjs][:x86_64][:tar_url]  = "https://phantomjs.googlecode.com/files/phantomjs-1.7.0-linux-x86_64.tar.bz2"
-default[:phantomjs][:x86_64][:checksum] = "28346300cedc0fbeea4264f124417f2d5ec50210"
-default[:phantomjs][:x86_64][:filename] = "phantomjs-1.7.0-linux-x86_64.tar.bz2"
+default[:phantomjs][:x86_64][:tar_url]  = "http://phantomjs.googlecode.com/files/phantomjs-1.8.0-linux-x86_64.tar.bz2"
+default[:phantomjs][:x86_64][:checksum] = "29804bfc357f961b4b17f9072b95f93c6be284ca"
+default[:phantomjs][:x86_64][:filename] = "phantomjs-1.8.0-linux-x86_64.tar.bz2"
 
 default[:phantomjs][:x86][:tar_url]     = "https://phantomjs.googlecode.com/files/phantomjs-1.7.0-linux-i686.tar.bz2"
 default[:phantomjs][:x86][:checksum]    = "4508abf55fef386a9079ed029b6d36d8719d2686"
