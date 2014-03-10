c656b9c5fc0aa80a9579ad32558fb7de698f81cd
jshou <joshuahou@gmail.com>
2014-03-10 11:45:37 -0700

    update phantomjs version

--- attributes/phantomjs.rb
+++ attributes/phantomjs.rb
@@ -1,8 +1,8 @@
-default[:phantomjs][:x86_64][:tar_url]  = "http://phantomjs.googlecode.com/files/phantomjs-1.8.0-linux-x86_64.tar.bz2"
-default[:phantomjs][:x86_64][:checksum] = "29804bfc357f961b4b17f9072b95f93c6be284ca"
-default[:phantomjs][:x86_64][:filename] = "phantomjs-1.8.0-linux-x86_64.tar.bz2"
+default[:phantomjs][:x86_64][:tar_url]  = "https://phantomjs.googlecode.com/files/phantomjs-1.9.2-linux-x86_64.tar.bz2"
+default[:phantomjs][:x86_64][:checksum] = "c78c4037d98fa893e66fc516214499c58228d2f9"
+default[:phantomjs][:x86_64][:filename] = "phantomjs-1.9.2-linux-x86_64.tar.bz2"
 
-default[:phantomjs][:x86][:tar_url]     = "https://phantomjs.googlecode.com/files/phantomjs-1.7.0-linux-i686.tar.bz2"
-default[:phantomjs][:x86][:checksum]    = "4508abf55fef386a9079ed029b6d36d8719d2686"
-default[:phantomjs][:x86][:filename]    = "phantomjs-1.7.0-linux-i686.tar.bz2"
+default[:phantomjs][:x86][:tar_url]     = "https://phantomjs.googlecode.com/files/phantomjs-1.9.2-linux-i686.tar.bz2"
+default[:phantomjs][:x86][:checksum]    = "9ead5dd275f79eaced61ce63dbeca58be4d7f090"
+default[:phantomjs][:x86][:filename]    = "phantomjs-1.9.2-linux-i686.tar.bz2"
 
--- metadata.rb
+++ metadata.rb
@@ -3,7 +3,7 @@ maintainer_email "shaun@substantial.com"
 license          "All rights reserved"
 description      "Installs/Configures phantomjs"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          "0.0.8"
+version          "0.0.9"
 
 %w{ debian ubuntu }.each do |os|
   supports os
