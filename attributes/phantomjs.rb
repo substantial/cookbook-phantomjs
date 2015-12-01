247dcad2f1bea54267fccd619f81c9d6b5c2d37b
Melanie VanderLugt <melanie@substantial.com>
2015-12-01 15:46:35 -0800

    use correct checksum for phantomjs tar file

--- attributes/phantomjs.rb
+++ attributes/phantomjs.rb
@@ -1,5 +1,5 @@
 default[:phantomjs][:x86_64][:tar_url]  = "https://s3.amazonaws.com/teamcity-buildagent/phantomjs-2.0.0-ubuntu-12.04.tar.bz2"
-default[:phantomjs][:x86_64][:checksum] = "4ea7aa79e45fbc487a63ef4788a18ef7"
+default[:phantomjs][:x86_64][:checksum] = "1b8479fe5166838494339d8a7af1003d"
 default[:phantomjs][:x86_64][:filename] = "phantomjs-2.0.0-ubuntu-12.04.tar.bz2"
 
 default[:phantomjs][:x86][:tar_url]     = "https://bitbucket.org/ariya/phantomjs/downloads/phantomjs-1.9.8-linux-i686.tar.bz2"
