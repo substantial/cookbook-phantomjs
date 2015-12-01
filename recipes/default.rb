8f54d49635d7488495c20b7b6df4dd652a932658
Mark Kornblum <mark.kornblum@gmail.com>
2015-12-01 15:09:04 -0800

    Restore i686 version to cookbook
    We decided to update this rather than taking it out altogether, although
    we don't currently have a version 2 binary built for i686, so installing
    on that platform will yield version 1.9.8.

--- recipes/default.rb
+++ recipes/default.rb
@@ -6,9 +6,15 @@
 #
 # All rights reserved - Do Not Redistribute
 
-tar_url = node[:phantomjs][:x86_64][:tar_url]
-tar_checksum = node[:phantomjs][:x86_64][:checksum]
-filename = node[:phantomjs][:x86_64][:filename]
+if node.kernel.machine == "x86_64"
+  tar_url = node[:phantomjs][:x86_64][:tar_url]
+  tar_checksum = node[:phantomjs][:x86_64][:checksum]
+  filename = node[:phantomjs][:x86_64][:filename]
+else
+  tar_url = node[:phantomjs][:x86][:tar_url]
+  tar_checksum = node[:phantomjs][:x86][:checksum]
+  filename = node[:phantomjs][:x86][:filename]
+end
 
 file_path = "#{Chef::Config[:file_cache_path]}/#{filename}"
 remote_file "Downloading phantomjs tar" do
