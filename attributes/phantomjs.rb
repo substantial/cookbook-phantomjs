fa9fcb8a03d0fdb69a072864d156c5a0c94d1cd2
Mark Kornblum <mark.kornblum@gmail.com>
2015-12-01 16:25:03 -0800

    Previous tar had one level too many of heirarchy

--- attributes/phantomjs.rb
+++ attributes/phantomjs.rb
@@ -1,5 +1,5 @@
 default[:phantomjs][:x86_64][:tar_url]  = "https://s3.amazonaws.com/teamcity-buildagent/phantomjs-2.0.0-ubuntu-12.04.tar.bz2"
-default[:phantomjs][:x86_64][:checksum] = "1b8479fe5166838494339d8a7af1003d"
+default[:phantomjs][:x86_64][:checksum] = "0ffdad604c4bd4a8ef40c5bc999b7340"
 default[:phantomjs][:x86_64][:filename] = "phantomjs-2.0.0-ubuntu-12.04.tar.bz2"
 
 default[:phantomjs][:x86][:tar_url]     = "https://bitbucket.org/ariya/phantomjs/downloads/phantomjs-1.9.8-linux-i686.tar.bz2"
